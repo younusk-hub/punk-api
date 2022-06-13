cad8ff8b8aba833ffe09d9f5dbe05b3c6b136add
Younus Khan <younuskhan99@hotmail.com>
2022-06-13 16:51:07 +0100

    started displaying info on card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,9 +3,23 @@ import React from 'react'
 const Cards = ({ arr }) => {
     console.log(arr);
 
+    const displayCards = arr.map(beer => {
+        
+        
+        return (
+            <div className='card'>
+                <h2 className='card__name'>{beer.name}</h2>
+                <p className='card__abv'>{beer.abv}</p>
+                <p className='card__first-brewed'>{beer.first_brewed}</p>
+                <p className='card__ph'>{beer.ph}</p>
+                <p className='card__description'>{beer.description}</p>
+            </div>
+        )
+    })
+
     return (
         <div>
-            
+            {displayCards}
         </div>
   )
 }
