9df6564170995511f72c9cd424a7a8e551793598
Younus Khan <younuskhan99@hotmail.com>
2022-06-22 14:53:50 +0100

    completed functionality and styled it

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,11 +2,6 @@ import React, {useState} from 'react'
 import "./Cards.scss"
 
 const Cards = ({ beer, index}) => {
-    const [ toggle, setToggle] = useState(false)
-
-    const toggleInfo = () => {
-        setToggle(!toggle)
-    }
     
     const foods = beer.food_pairing.map(element => {
         return (
@@ -14,43 +9,29 @@ const Cards = ({ beer, index}) => {
         )
     });
 
-    const falseToggle = (
-        <div className='card' key={index}>
-            <div>
-                <h2 className='card__name'>Name: {beer.name}</h2>
-                <p className='card__id'>id: {beer.id}</p>
-            </div>
-            <img className='card__image' src={beer.image_url} alt={beer.name} />
-            <div className='card__info'>
-                <p className='card__info__abv'>ABV: {beer.abv}</p>
-                <p className='card__info__first-brewed'>First brewed: {beer.first_brewed}</p>
-                <p className='card__info__ph'>PH: {beer.ph}</p>
-            </div>
-            <button onClick={toggleInfo} className='card__button'>More Info</button>
-        </div>
-    )
-
-    const trueToggle = (
-        <div className='card' key={index}>
-            <div>
-                <h2 className='card__name'>Name: {beer.name}</h2>
-                <p className='card__id'>id: {beer.id}</p>
-            </div>
-            
-            <div className='card__info'>
-                <p className='card__info__tagline'>{beer.tagline}</p>
-                <p className='card__info__volume'>{beer.volume.value} {beer.volume.unit}</p>
-                <ul className='card__info__list'>Great with: {foods}</ul>
-                <p className='card__info__description'>{beer.description}</p>
-               
-            </div>
-             <button onClick={toggleInfo} className='card__button'>Go Back</button>
-        </div>
-    )
-
-
     return (
-        <>{toggle ?  trueToggle : falseToggle}</>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div className='card flip-card-front' key={index}>
+                    <h2 className='card__name'>{beer.name}</h2>
+                    <img className='card__image' src={beer.image_url} alt={beer.name} />
+                    <div className='card__info'>
+                        <p className='card__info__abv'>ABV: {beer.abv}</p>
+                        <p className='card__info__first-brewed'>First brewed: {beer.first_brewed}</p>
+                        <p className='card__info__ph'>PH: {beer.ph}</p>
+                    </div>
+                </div>
+                <div className='card flip-card-back' key={index}>
+                    <h2 className='card__name'>{beer.name}</h2>
+                    <div className='card__info'>
+                        <p className='card__info__tagline'>{beer.tagline}</p>
+                        <p className='card__info__volume'>{beer.volume.value} {beer.volume.unit}</p>
+                        <ul className='card__info__list'>Great with: {foods}</ul>
+                        <p className='card__info__description'>{beer.description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
